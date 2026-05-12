c49f61133d5ea21b8f0c18e4578ec2244dcf8542
Pratik Anand <pratikanand0512@gmail.com>
2026-05-12 23:11:32 +0530

    Task2: change line and Concatnation

---
+++ Day1/task2.py
@@ -0,0 +1,10 @@
+# Suppose we want to print in the console in different line then we use \n (backslash)
+
+# for example
+
+print("My name is Anand\nI am learning python")
+
+
+# if we want to contcatnate the two strings then we use + sign
+
+print("Pratik " + "Anand")
