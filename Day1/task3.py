70268990002504229058598aecb1d50b28dbe2f4
Pratik Anand <pratikanand0512@gmail.com>
2026-05-14 09:55:34 +0530

    Input

---
+++ Day1/task3.py
@@ -0,0 +1,18 @@
+# to take input from user we use input function: input("a prompt from user")
+
+# for example
+
+#input("What is your Name?")
+
+# input function always returns string 
+
+# input("Enter age") # return string not numbers 
+
+# Now we want to print the name of the use with Hello 
+
+print("My name is " + input("What is your name?\n") + " and my age is " + input("Enter age: \n") + " Years old.")
+
+# on the above code input function returns string not numbers 
+
+# print("Hello " + input("Name please: ") + "!")
+
---
+++ Day1/task4.py
@@ -0,0 +1,39 @@
+# Varialble 
+
+# Variables are used to store the vlue 
+
+# For example: 
+
+name = input("What is your Name?")
+
+print(name)
+
+# in the above name holds the value whaterver the user enter, and later we can print the value.
+
+# the variable  name can be changed also
+
+# For example: 
+
+name = "Ananad"
+
+print(name)
+
+# At first the user enter the name: Pratik and we print that name however later we changed that name
+
+# name to Ananad and then we print the  same variable name, the value has changed 
+
+# Suppose we want to count the characters of something the use input, then we can do this by using len function 
+
+# For example: 
+
+# I want to count the numbers of characters of my name
+
+print(len(input("What is your name?")))
+
+# we can also split the above code with the use of variables and make it more readable 
+
+userName = input("What is your name?")
+
+lenght_of_UserName = len(userName)
+
+print(lenght_of_UserName)
