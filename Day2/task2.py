29f9b988c3516c146950601a467e5d8d8be6d071
Pratik Anand <pratikanand0512@gmail.com>
2026-05-15 10:27:27 +0530

    Type Conversion

---
+++ Day2/task2.py
@@ -0,0 +1,44 @@
+# Type Errors:  Checking and Conversion
+
+# to check the data type we use type
+
+# for example we want to know the data types of "123"
+
+type("123")
+
+print(type("123")) # string
+
+# data type of 123
+
+print(type(123)) # integer
+
+# data type of 123.45
+
+print(type(123.45))
+
+# data typr of boolean: True or Flase
+
+print(type(True))
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+
+# TYPE CONVERSION
+
+# If we want to convert one data type to another then we can do this and this is known as type casting
+
+# For example:
+
+print("123" + "456") # it will be concatnated to give the result 123456
+
+print(int("123") + int("456")) # it will convert the string into numbers and then add
+
+# we can use 
+
+int()
+
+str()
+
+float()
+
+bool()
