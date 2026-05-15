32b1c9a686606f5c98f0e9126e36296b8ec74c83
Pratik Anand <pratikanand0512@gmail.com>
2026-05-15 09:57:41 +0530

    Day2: Data types: Number String Float

---
+++ Day2/task1.py
@@ -0,0 +1,49 @@
+# Data types Numbers Operations Type conversions fstrings
+
+# subscripting
+
+print("Hello"[0])
+
+# the above code helps in printing the indices starting from 0 and if we want to print 
+
+# from last then we can use the negative sign
+
+# for example:
+
+print("Hello"[-1]) # it will print the last character 
+
+# Numbers
+
+print("123" + "456") # this will be treated as string and it will be concatnated 
+
+# to print numbers 
+
+print(123)
+
+# to add the number
+
+print(123+456)
+
+# For large numbers
+
+print(1234567890)
+
+# if we want to make it more readable then we use comma however when we use comma it will give an error
+
+# hence we use underscore to make it readable on code, in console it will print normally 
+
+print(123_456_789)
+
+# Float 
+
+print(3.14)
+
+# Boolean
+
+print(True)
+
+print(False)
+
+# len(1234) # it will give error because the len function takes the string only
+
+print(len("12340")) # it will count the characters and then it will print 
