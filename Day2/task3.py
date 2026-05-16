160d09ef06fac6302088141e5e08aac6427edd50
Pratik Anand <pratikanand0512@gmail.com>
2026-05-16 19:09:42 +0530

    Mathematical Operations

---
+++ Day2/task3.py
@@ -0,0 +1,23 @@
+# mathematical operation
+
+# + - * /
+
+print(3 + 3)
+
+print(10 - 5)
+
+print(4 * 4)
+
+print(type(15 / 5)) # given floating point number even divides completely 
+
+print(type( 15 // 5)) # returns integer number 
+
+print(4 ** 2) # exponent 
+
+# Priority 
+
+# P()E**D/M*A+S- calculation from left to right 
+
+print(3 * 3 + 3 / 3 -3) # print 7
+
+print(3 * (3 + 3) / 3 -3) # because of parenthesis it will be executed first 
