e39dcde935dc5aa854471b81eac7a817119e1f75
Pratik Anand <pratikanand0512@gmail.com>
2026-05-20 10:32:27 +0530

    Day3: If Else

---
+++ Day3/task1IfElse.py
@@ -0,0 +1,17 @@
+# if else 
+
+# syntax
+
+# if condition:
+#     print("")
+# else:
+#     print("")
+
+print("Welcome to the roller coaster!")
+
+height = float(input("What is your height in  cm?"))
+
+if height>120:
+    print("You can ride the roller coaster!")
+else:  
+    print("Sorry, you have to grow taller before you can ride.")
