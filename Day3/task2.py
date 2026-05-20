e5811131866bce2384976587e00c988689042e65
Pratik Anand <pratikanand0512@gmail.com>
2026-05-20 10:43:54 +0530

    modulo operator

---
+++ Day3/task2.py
@@ -0,0 +1,10 @@
+# modulo operator % - gives remainder
+
+# check even and odd
+
+number =  int(input("Enter number to Check if it is even or odd.\n"))
+
+if number%2 == 0:
+    print(f"{number} is even")
+else:
+    print(f"{number} is odd.")
