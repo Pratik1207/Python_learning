6ce078e3526768981ae693636c023d3cdcca4570
Pratik Anand <pratikanand0512@gmail.com>
2026-05-21 10:55:26 +0530

    syntax: multiple if and else and elif

---
+++ Day3/task3.py
@@ -0,0 +1,12 @@
+# elif statement - nested if else
+
+# Syntax
+
+# if condition:
+#     do something
+# elif condition:
+#     do something
+# elif condition:
+#     do something
+# else
+#     do  something
