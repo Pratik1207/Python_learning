c38ca431c8b77a7ce7a0182efe4629d1be80d40c
Pratik Anand <pratikanand0512@gmail.com>
2026-06-06 22:50:42 +0530

    Logical operators

---
+++ Day3/task5.py
@@ -0,0 +1,23 @@
+# logical operator 
+
+# and
+
+# or
+
+# not
+
+print(True and True)
+
+print(True and False)
+
+
+print(True or True)
+
+
+print(False or True)
+
+
+print(False or False)
+
+print(not True)
+
