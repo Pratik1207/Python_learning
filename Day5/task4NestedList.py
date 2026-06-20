370e3e52daf9479aa20735f2bf089a0ff4acbaa3
Pratik Anand <pratikanand0512@gmail.com>
2026-06-20 22:21:37 +0530

    nestede list

---
+++ Day5/task4NestedList.py
@@ -0,0 +1,12 @@
+# nested list 
+
+fruits = ["apple", "mango", "banana"]
+
+vegetable = ["potato", "tomato"]
+
+NestedList = [fruits, vegetable]
+
+print(NestedList)
+
+
+print(NestedList[1][1]) # tomato
